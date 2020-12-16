faaacbf445f78a6aa94ff2596d825dcd74f2cca8
Aydayll <makenovaay23@gmail.com>
2020-12-16 10:43:36 +0600

    start the react project

---
+++ src/components/header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => {
+    return(
+        <header className = "header">
+           <div className="container">
+               <div className="header__row">
+                   <div className="header__logo">ryxutcfygv</div>
+                       <div className="header__nav">
+                          <Items />
+                          </div>
+                       <div className="header__btn">szdfcgvh;bj</div>
+               </div>
+           </div>
+        </header>
+    );
+};
+
+export default Header ;
---
+++ src/components/header/items.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+ 
+function Items() {
+   return(
+      <nav>
+          <ul>
+              
+          </ul>
+      </nav>
+   )
+}
