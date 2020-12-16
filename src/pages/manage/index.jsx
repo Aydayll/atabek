6df77ea2e70d87022cb8df2517bf45504b357613
Aydayll <makenovaay23@gmail.com>
2020-12-17 01:58:59 +0600

    add the icon-plus and style manage

--- src/pages/manage/index.jsx
+++ src/pages/manage/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Plus from '../../assets/plus.svg'
 import './style.scss';
 
 const Manage = () => {
     return (
         <section className="manage">
-            <div className="manag__top">
+            <div className="manage__top">
                 <div className="manage__filter">FILTER</div>
                 <label className="manage__search">
                     <input type="text" placeholder='Search users by name, id' />
                 </label>
-                <button className="manage__add-user"></button>
+                <button className="manage__add-user">
+                <img src={Plus} alt=""/>
+                    ADD USER
+                </button>
             </div>
             <table className="manage__users"></table>
         </section>
