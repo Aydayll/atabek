2428c039a2a54ec4e5b981dee930c1c5a121e961
Aydayll <makenovaay23@gmail.com>
2020-12-17 22:30:22 +0600

    style the manage items

--- src/pages/manage/index.jsx
+++ src/pages/manage/index.jsx
@@ -26,14 +26,15 @@ const Manage = () => {
                 <th className = "col-2 manage__headers">Email Address</th>
                 <th className = "col-2 manage__headers">Account Created</th>
                 </tr>
-                <tr>
-                    <td>
-                        <img src={Avatar} alt="Avatar"/>
+                <tr className="manage__row">
+                    <td className=" col-2 user__row">
+                        <img className="user__avatar" src={Avatar} alt="Avatar"/>
+                        <span className="user__name">Makenova Ayday</span>
                     </td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td className="col-3 manage__headers" >DEV63016762</td>
+                    <td className="col-3 manage__headers" >1-(618)312-3065</td>
+                    <td className="col-3 manage__headers" >rsnyder@blogspan.gov</td>
+                    <td className="col-3 manage__headers user__date" >04/28/2018</td>
                 </tr>
             </table>
         </section>
