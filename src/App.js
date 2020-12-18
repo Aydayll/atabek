e217a88d19007bd80877be18e1d1dabf27eda533
Aydayll <makenovaay23@gmail.com>
2020-12-19 04:23:55 +0600

    adding the newUser

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import Header from './components/header/';
 import Sidebar from './components/sidebar';
 import Manage from './pages/manage';
-import axios from 'axios';
+
 function App() {
-  const [userData, setUsersData] = React.useState([]);
-  React.useEffect(async () =>{
-    await axios
-    .get(`http://localhost:3000/database.json`)
-    .then((response) => response)
-    .then(({ data}) => setUsersData(data.users));
-  }, []);
   return (
     <div className="App">
       <Header />
       <Sidebar />
-      <Manage  data = {userData}/>
+      <Manage />
     </div>
   );
 }
--- src/pages/manage/index.jsx
+++ src/pages/manage/index.jsx
@@ -2,48 +2,66 @@ import React from 'react';
 import Plus from '../../assets/plus.svg'
 import './style.scss';
 import Item from './items';
+import axios from 'axios';
 
-const Manage = ({data}) => {
-    const [usersData, setUsersData] = React.useState(data);
-    const [activeModal, setActive] = React.useState(false);
-    const handleClick = () =>{
-        console.log(data)
+const Manage = () => {
+    const [usersData, setUsersData] = React.useState([]);
+    const [newUser, setNewUser] = React.useState({
+        id: '',
+        avatar: '',
+        fullName: '',
+        userID: '',
+        email: '',
+        phone: '',
+        dateOfRegistry: '',
+    });
+    const [activeModal, setActiveModal] = React.useState(false);
+
+    const handleClick = () => {
+        setActiveModal(activeModal ? false : true);
     };
+
+    React.useEffect(async () => {
+        await axios
+            .get(`http://localhost:3000/database.json`)
+            .then((response) => response)
+            .then(({ data }) => setUsersData(data.users));
+    }, []);
     return (
         <section className="manage">
             {activeModal && (
-            <div className="modal">
-                <form>
-                    <input type="text" placeholder = 'Введите Имя Пользователя'/>
-                    <input type="text" placeholder = 'Введите картинку'/>
-                    <input type="text" placeholder = 'Введите айди'/>
-                    <input type="text" placeholder = 'Введите емаил'/>
-                    <input type="text" placeholder = 'Введите номер'/>
-                </form>
-            </div>
+                <div className="modal"> 
+                    <form>
+                        <input value={newUser.fullName} type="text" placeholder='Введите Имя Пользователя' />
+                        <input value={newUser.avatar} type="text" placeholder='Введите путь к картинке' />
+                        <input value={newUser.id} type="text" placeholder='Введите ID' />
+                        <input value={newUser.phone} type="text" placeholder='Введите номер телефона' />
+                        <input value={newUser.email} type="text" placeholder='Введите почту пользователя' />
+                    </form>
+                </div>
             )}
             <div className="manage__top">
                 <div className="row">
-                <div className="manage__filter">FILTER</div>
-                <label className="manage__search">
-                    <input type="text" placeholder='Search users by name, id' />
-                </label>
+                    <div className="manage__filter">FILTER</div>
+                    <label className="manage__search">
+                        <input type="text" placeholder='Search users by name, id' />
+                    </label>
                 </div>
-                <button onClick={handleClick}className="manage__add-user">
-                <img src={Plus} alt="plus"/>
+                <button onClick={handleClick} className="manage__add-user">
+                    <img src={Plus} alt="plus" />
                     ADD USER
                 </button>
             </div>
             <table className="manage__users">
                 <tr className="manage__row">
-                <th className = "col-2 manage__headers">Selected 2 users</th>
-                <th className = "col-2 manage__headers">User ID</th>
-                <th className = "col-2 manage__headers">Phone Number</th>
-                <th className = "col-2 manage__headers">Email Address</th>
-                <th className = "col-2 manage__headers">Account Created</th>
+                    <th className="col-2 manage__headers">Selected 2 users</th>
+                    <th className="col-2 manage__headers">User ID</th>
+                    <th className="col-2 manage__headers">Phone Number</th>
+                    <th className="col-2 manage__headers">Email Address</th>
+                    <th className="col-2 manage__headers">Account Created</th>
                 </tr>
-                <Item users={data}/>
-                
+                <Item users={usersData} />
+
             </table>
         </section>
     );
