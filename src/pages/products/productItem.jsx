cbce415c278a847a55a56a94c60441af9f021529
Aydayll <makenovaay23@gmail.com>
2020-12-31 03:16:59 +0600

    add some items for product

---
+++ src/pages/products/productItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import timeIcon from '../../assets/time-icon.svg';
+import downloadIcon from '../../assets/download-icon.svg';
+
+
+
+const ProductItem = ({item}) => {
+  return (
+    <div className='col-4 products__column'>
+      <div className='products__item'>
+        <div><div className='products__icon'>
+          <img
+            src={item.productLogo}
+            alt='product__logo'
+          />
+        </div>
+        <h3 className='products__title'>{item.productName}</h3>
+        <p className='products__description'>
+         {item.productDescription}
+        </p></div>
+        <div className='products__footer row'>
+          <div>
+            <img src={timeIcon} alt='time icon' />
+            
+          </div>
+          <div>
+						<a href="https://slack.com/ssb/download-win">
+						<img src={downloadIcon} alt="download icon"/>
+						</a>
+            <span>{item.downloads} downloads</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
