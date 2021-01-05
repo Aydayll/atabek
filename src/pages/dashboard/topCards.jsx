1adee68dcdbeb1eb340d4b8b073ea8acc3509cff
Aydayll <makenovaay23@gmail.com>
2021-01-05 10:32:15 +0600

    start the dashboard

---
+++ src/pages/dashboard/topCards.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const topCards = () => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-3">
+                    <div className="top-card__item">
+                        <span className="top-card__suptitle"></span>
+                        <h3 className="top-card__title"></h3>
+                    </div>
+                    </div>
+                <div className="col-3"><div className="top-card__item">
+                    </div></div>
+                <div className="col-3"><div className="top-card__item">
+                    </div></div>
+                <div className="col-3"><div className="top-card__item">
+                    </div></div>
+            </div>
+        </div>
+    )
+}
+
+export default topCards
