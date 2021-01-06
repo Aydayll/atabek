3c18eb35c22b2630d27b33a1b8e2abf43686735f
Aydayll <makenovaay23@gmail.com>
2021-01-06 10:41:16 +0600

    add the verticalBar

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopCards from './topCards';
+import VerticalBar from './verticalBar';
 import './style.scss';
 const Dashboard = () => {
   return (
     <section>
       <div className='section__container'>
         <TopCards />
+        <VerticalBar />
       </div>
     </section>
   );
